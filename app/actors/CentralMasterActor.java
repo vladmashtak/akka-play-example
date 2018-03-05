3268b40e68a1baf851d06080ddbeedd4cce9f840
Vladislav <vladmashtak@gmail.com>
2018-03-05 16:49:46 +0200

    add submodule

--- app/actors/CentralMasterActor.java
+++ app/actors/CentralMasterActor.java
@@ -3,6 +3,8 @@ package actors;
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.cluster.Cluster;
+import akka.cluster.ClusterEvent.MemberUp;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
 import akka.routing.FromConfig;
@@ -12,28 +14,38 @@ import play.libs.akka.InjectedActorSupport;
 
 public class CentralMasterActor extends AbstractActor implements InjectedActorSupport {
     private final ActorRef engineNode;
+    private final Cluster cluster;
     private final LoggingAdapter logger = Logging.getLogger(getContext().getSystem(), this);
 
     @Inject
     public CentralMasterActor(ActorSystem system) {
+        cluster = Cluster.get(system);
         engineNode = system.actorOf(FromConfig.getInstance().props(), "EngineNode");
     }
 
-    // subscribe to cluster changes
     @Override
     public void preStart() {
-        logger.info("CentralMasterImpl started");
+        logger.info("Central started");
+        cluster.subscribe(getSelf(), MemberUp.class);
     }
 
-    // re-subscribe when restart
     @Override
     public void postStop() {
-        logger.info("CentralMasterImpl stopped");
+        logger.info("Central stopped");
+        cluster.unsubscribe(getSelf());
     }
 
+/*    @Override
+    public void onReceive(Object msg) throws Exception {
+        unhandled(msg);
+    }*/
+
     @Override
-    public AbstractActor.Receive createReceive() {
+    public Receive createReceive() {
         return receiveBuilder()
+                .match(MemberUp.class, mUp -> {
+                    logger.info("MemberUp: " + mUp.member().toString());
+                })
                 .matchEquals("Hello", s -> {
                     logger.info("Message: " + s);
                     engineNode.tell(s, getSelf());
@@ -43,5 +55,4 @@ public class CentralMasterActor extends AbstractActor implements InjectedActorSu
                 })
                 .build();
     }
-
 }
--- build.sbt
+++ build.sbt
@@ -1,15 +1,16 @@
-name := """akka-central"""
+organization := "Metrotek"
 
-organization := "central"
+name := "CentralMaster"
 
-version := "1.0-SNAPSHOT"
+version := "1.0"
 
 scalaVersion := "2.12.2"
 
 val akkaVersion = "2.5.10"
 
-lazy val root = (project in file(".")).enablePlugins(PlayJava)
+lazy val messages = project.in(file("akka-play-spring-messages"))
 
+lazy val root = (project in file(".")).dependsOn(messages).enablePlugins(PlayJava)
 libraryDependencies ++= Seq(
   guice,
   "com.typesafe.akka" %% "akka-remote" % akkaVersion,
